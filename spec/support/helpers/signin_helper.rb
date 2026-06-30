4fbd82d2833d8db998e6a7edcbf8f80e8da7046c
SilasNS <silasschlax@gmail.com>
2026-06-30 14:55:32 -0400

    Add missing helpers and create signin_helper

---
+++ spec/support/helpers/signin_helper.rb
@@ -0,0 +1,8 @@
+module SigninHelper
+  def log_in_user(user)
+    visit new_session_path
+    fill_in "email_address", with: user.email_address
+    fill_in "password", with: user.password
+    click_button "Sign In"
+  end
+end
