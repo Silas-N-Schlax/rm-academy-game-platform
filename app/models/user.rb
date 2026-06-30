fb93b2db21883fc13afd7ae352d052df19bf8123
SilasNS <silasschlax@gmail.com>
2026-06-30 14:56:24 -0400

    Add basic validation for creating a new profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  attribute :confirm_password
+
+  validates :name, presence: true
+  validates :email_address, presence: true, uniqueness: { case_insensitive: true }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { in: 6..20 }, comparison: { equal_to: :confirm_password }
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'returns true if given valid input' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'returns false if name is not valid' do
+      user = build(:user, name: nil)
+      expect(user).to be_invalid
+    end
+
+    it 'returns false if email is not valid' do
+      user = build(:user, email_address: 'test')
+      expect(user).to be_invalid
+    end
+
+    it 'returns false if the password is invalid' do
+      user = build(:user, password: nil)
+      expect(user).to be_invalid
+    end
+
+    it 'returns false if the password is too long' do
+      user = build(:user, password: '123')
+      expect(user).to be_invalid
+    end
+
+    it 'returns false if the password is too short' do
+      user = build(:user, password: '1234567891013829318972398724984')
+      expect(user).to be_invalid
+    end
+
+    it 'returns false if password and confirm_password do not match' do
+      user = build(:user, password: '123456')
+      expect(user).to be_invalid
+    end
+  end
 end
