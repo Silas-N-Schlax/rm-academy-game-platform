00860290f66dac8a2cc4345be1184efcfc877490
SilasNS <silasschlax@gmail.com>
2026-06-30 14:57:07 -0400

    Add more params to user factory bot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :user do
+    name { "player" }
     email_address { "user@example.com" }
     password { "password" }
+    confirm_password { "password" }
   end
 end
