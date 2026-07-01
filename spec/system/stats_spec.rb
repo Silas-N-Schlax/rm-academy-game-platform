67c3b2ef536330d733982e1b3270a6ef00c08dcc
SilasNS <silasschlax@gmail.com>
2026-07-01 08:23:15 -0400

    Fix failing test from no-loging- in

--- spec/system/stats_spec.rb
+++ spec/system/stats_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'Stats', type: :system do
-  let!(:user) { create(:user) }
-  before { log_in_user(user) }
   it 'shows the rules' do
+    user = create(:user)
+    log_in_user(user)
     visit stats_path
     expect(page).to have_content 'Stats'
   end
