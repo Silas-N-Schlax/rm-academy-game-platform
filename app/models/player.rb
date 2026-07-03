bd0ae45341c40c35d056d535af93cf9b65887941
SilasNS <silasschlax@gmail.com>
2026-07-02 20:16:24 -0400

    Implement players as a join table for users/games

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  validates :game, uniqueness: { scope: :user, message: "You cannot join this game" }
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
+  describe 'validations' do
+    let(:game) { build :game }
+    let(:user) { build :user }
+    let(:expected_message) { "Game You cannot join this game" }
+    it 'does not allow user to join twice' do
+      valid_player = build(:player, game:, user:)
+      expect(valid_player).to be_valid
+      valid_player.save
+      invalid_player = build(:player, game:, user:)
+      expect(invalid_player).to be_invalid
+      expect(invalid_player.errors.full_messages.to_sentence).to eq expected_message
+    end
+  end
 end
