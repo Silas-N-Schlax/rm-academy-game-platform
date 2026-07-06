603f3e1a34dc0c45a40088806a10ab007ecaa0db
SilasNS <silasschlax@gmail.com>
2026-07-06 10:41:59 -0400

    Create game_state column in games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_02_215532) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_06_144139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_02_215532) do
     t.datetime "created_at", null: false
     t.datetime "finished_at"
     t.integer "game_size", null: false
+    t.jsonb "game_state"
     t.string "game_type", null: false
     t.string "name", null: false
     t.datetime "started_at"
