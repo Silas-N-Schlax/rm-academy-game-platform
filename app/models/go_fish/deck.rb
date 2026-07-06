d451940bc133bfefeb3a6ab4c77f192626c60ad8
SilasNS <silasschlax@gmail.com>
2026-07-06 12:00:35 -0400

    Port deck from sinatra app

---
+++ app/models/go_fish/deck.rb
@@ -0,0 +1,32 @@
+module GoFish
+  class Deck
+    attr_accessor :cards
+
+    def initialize
+      @cards = GoFish::Card::SUITS.flat_map do |suit|
+        GoFish::Card::RANKS.map do |rank|
+          GoFish::Card.new(rank, suit)
+        end
+      end
+    end
+
+    def top_card
+      cards.shift
+    end
+
+    def shuffle_deck
+      new_deck = cards.dup.shuffle!
+      shuffle_deck if new_deck == cards
+
+      self.cards = new_deck
+    end
+
+    def cards_left
+      cards.size
+    end
+
+    def empty?
+      cards.empty?
+    end
+  end
+end
