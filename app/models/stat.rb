59a4985215171b826be185847b089613022e31f8
SilasNS <silasschlax@gmail.com>
2026-07-06 09:07:38 -0400

    Switch from .size to .count

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,14 +1,14 @@
 class Stat
   def total_games(user)
-    User.find_by(id: user.id).games.size
+    User.find_by(id: user.id).games.count
   end
 
   def total_wins(user)
-    Player.where(user_id: user.id, winner: true).size
+    Player.where(user_id: user.id, winner: true).count
   end
 
   def total_losses(user)
-    Player.where(user_id: user.id, winner: nil).size
+    Player.where(user_id: user.id, winner: nil).count
   end
 
   def total_average(user)
@@ -18,19 +18,19 @@ class Stat
   end
 
   def total_games_by_game(user, type: "Go Fish")
-    User.find_by(id: user.id).games.where(game_type: type).size
+    User.find_by(id: user.id).games.where(game_type: type).count
   end
 
   def total_wins_by_game(user, type: "Go Fish")
     Player.where(user_id: user.id, winner: true).each  do |player|
       player.game.game_type == type
-    end.size
+    end.count
   end
 
   def total_losses_by_game(user, type: "Go Fish")
     Player.where(user_id: user.id, winner: nil).each  do |player|
       player.game.game_type == type
-    end.size
+    end.count
   end
 
   def total_average_by_game(user, type: "Go Fish")
