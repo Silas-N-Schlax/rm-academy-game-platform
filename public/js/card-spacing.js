6e099845f6b446b2b83bef9b5d81ffea52e8c498
SilasNS <silasschlax@gmail.com>
2026-07-14 13:06:54 -0400

    Update card margin from JS to CSS

--- public/js/card-spacing.js
+++ public/js/card-spacing.js
@@ -1,41 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-  changeOffset()
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//   changeOffset()
+// })
 
-window.addEventListener('resize', () => {
-  changeOffset()
-})
+// window.addEventListener('resize', () => {
+//   changeOffset()
+// })
 
 
-function changeOffset() {
-  const blackListedCards = ['x-large', '2x-small']
-  const cardCollections = document.querySelectorAll('.card-collection')
-  cardCollections.forEach((collection) => {
-    cards = collection.querySelectorAll('.playing-card')
-    let cardType = cards[0]
-    if (cardType) {
-      cardType = cardType.className.split('--')
-      if (blackListedCards.includes(cardType[cardType.length - 1])) return
-    }
-    if (cards.length == 0) return
+// function changeOffset() {
+//   const blackListedCards = ['x-large', '2x-small']
+//   const cardCollections = document.querySelectorAll('.card-collection')
+//   cardCollections.forEach((collection) => {
+//     cards = collection.querySelectorAll('.playing-card')
+//     let cardType = cards[0]
+//     if (cardType) {
+//       cardType = cardType.className.split('--')
+//       if (blackListedCards.includes(cardType[cardType.length - 1])) return
+//     }
+//     if (cards.length == 0) return
 
-    let minOffset = 15 // desktop min offset
-    if (window.innerWidth < 800) minOffset = 3 // mobile min offset
+//     let minOffset = 15 // desktop min offset
+//     if (window.innerWidth < 800) minOffset = 3 // mobile min offset
 
-    const cardWidth = cards[0].width
-    const cardsLength = cards.length
-    let containerWidth = collection.offsetWidth
+//     const cardWidth = cards[0].width
+//     const cardsLength = cards.length
+//     let containerWidth = collection.offsetWidth
 
-    let offset = 0
-    while ((cardsLength * cardWidth) - (offset * cardsLength) > containerWidth - cardWidth) {
-      offset += 1
-    }
+//     let offset = 0
+//     while ((cardsLength * cardWidth) - (offset * cardsLength) > containerWidth - cardWidth) {
+//       offset += 1
+//     }
 
-    if (offset < minOffset) offset = minOffset
+//     if (offset < minOffset) offset = minOffset
 
-    collection.style.marginLeft = `${offset}px`
-    cards.forEach((card) => {
-      card.style.marginLeft = `${offset * -1}px`
-    })
-  })
-}
+//     collection.style.marginLeft = `${offset}px`
+//     cards.forEach((card) => {
+//       card.style.marginLeft = `${offset * -1}px`
+//     })
+//   })
+// }
