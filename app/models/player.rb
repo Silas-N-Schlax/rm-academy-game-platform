f92789fcd46a40dfb3dc605997b76ee86643fd19
SilasNS <107065641+Silas-N-Schlax@users.noreply.github.com>
2026-07-14 15:16:37 -0400

    Turbo (#9)
    * Update card margin from JS to CSS
    
    * Implement basics of Turbo for game index & show

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,18 @@ class Player < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
+  after_create_commit :update_games_list
+
   validates :game, uniqueness: { scope: :user, message: "You cannot join this game" }
+
+  private
+
+  def update_games_list
+    broadcast_replace_to(
+      "games",
+      partial: "application/game_card",
+      locals: { item: self.game, post: true },
+      target: self.game
+    )
+  end
 end
