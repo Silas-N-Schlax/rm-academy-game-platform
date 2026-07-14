d387ce7cbc2f2ba34beecd352ea098cc0da123a5
SilasNS <silasschlax@gmail.com>
2026-07-14 17:28:03 -0400

    Implement ArchiveGameJon and schedule Cron

--- app/jobs/archive_game_job.rb
+++ app/jobs/archive_game_job.rb
@@ -1,7 +1,20 @@
 class ArchiveGameJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    archive_stale_games
+  end
+
+  private
+
+  def archive_stale_games
+    Game.all.where(archived_at: nil).each do |game|
+      game.archived_at = Time.current if stale_game?(game.updated_at)
+      game.save
+    end
+  end
+
+  def stale_game?(updated_at)
+    updated_at < 2.days.ago
   end
 end
