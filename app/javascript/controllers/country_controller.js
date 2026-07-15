707c3afc1400586476c4cb1f00296bd89340edd3
SilasNS <silasschlax@gmail.com>
2026-07-15 18:03:40 -0400

    Fix controllers and setup first controller

---
+++ app/javascript/controllers/country_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from '@hotwired/stimulus'
+import { patch } from '@rails/request.js'
+export default class extends Controller {
+  static values = { url: String, count: Number }
+  async perform() {
+    const body = new FormData(this.element)
+    const response = await patch(this.urlValue, { body, responseKind: 'turbo-stream' })
+    if (response.ok) this.countValue += 1
+  }
+}
