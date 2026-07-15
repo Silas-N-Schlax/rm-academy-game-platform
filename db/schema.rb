ab47b8884662a5fc68f2bb891b38365b674404eb
SilasNS <silasschlax@gmail.com>
2026-07-15 18:04:11 -0400

    Add address related content to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_14_202308) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_15_213812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -175,11 +175,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_14_202308) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "city"
+    t.string "country"
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "name", null: false
     t.string "password_digest", null: false
+    t.string "state"
+    t.string "street_address"
     t.datetime "updated_at", null: false
+    t.string "zip_code"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
