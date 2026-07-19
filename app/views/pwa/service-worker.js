475497bb9f2af2e652d5abafafab269df588ab17
SilasNS <silasschlax@gmail.com>
2026-07-19 15:29:07 -0400

    Setup basic service worker for PWA to listen for offline

--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -1,26 +1,82 @@
 // Add a service worker for processing Web Push notifications:
 //
-self.addEventListener("push", async (event) => {
-  const { title, options } = await event.data.json()
-  event.waitUntil(self.registration.showNotification(title, options))
-})
+// self.addEventListener("push", async (event) => {
+//   const { title, options } = await event.data.json()
+//   event.waitUntil(self.registration.showNotification(title, options))
+// })
 
-self.addEventListener("notificationclick", function(event) {
-  event.notification.close()
+// self.addEventListener("notificationclick", function(event) {
+//   event.notification.close()
+//   event.waitUntil(
+//     clients.matchAll({ type: "window" }).then((clientList) => {
+//       for (let i = 0; i < clientList.length; i++) {
+//         let client = clientList[i]
+//         let clientPath = (new URL(client.url)).pathname
+
+//         if (clientPath == event.notification.data.path && "focus" in client) {
+//           return client.focus()
+//         }
+//       }
+
+//       if (clients.openWindow) {
+//         return clients.openWindow(event.notification.data.path)
+//       }
+//     })
+//   )
+// })
+
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    clients.matchAll({ type: "window" }).then((clientList) => {
-      for (let i = 0; i < clientList.length; i++) {
-        let client = clientList[i]
-        let clientPath = (new URL(client.url)).pathname
+    addResourcesToCache([
+      "/offline"
+    ]),
+  );
+  self.skipWaiting()
+  console.log('installing')
+});
 
-        if (clientPath == event.notification.data.path && "focus" in client) {
-          return client.focus()
-        }
-      }
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match('/offline');
+      })
+    );
+  }
+});
 
-      if (clients.openWindow) {
-        return clients.openWindow(event.notification.data.path)
-      }
-    })
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((key) => key !== 'v1').map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
   )
 })
+
+
+// const deleteCache = async (key) => {
+//   await caches.delete(key);
+// };
+
+// const deleteOldCaches = async () => {
+//   const cacheKeepList = ["v2"];
+//   const keyList = await caches.keys();
+//   const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
+//   await Promise.all(cachesToDelete.map(deleteCache));
+// };
+
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(deleteOldCaches());
+// });
