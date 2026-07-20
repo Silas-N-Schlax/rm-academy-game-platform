97ee52304db93b4d75c231bc203f9bd5307832cb
SilasNS <silasschlax@gmail.com>
2026-07-20 12:25:50 -0400

    Remove dead code in service worker that was not gen

--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -64,19 +64,3 @@ self.addEventListener("activate", (event) => {
       .then(() => self.clients.claim())
   )
 })
-
-
-// const deleteCache = async (key) => {
-//   await caches.delete(key);
-// };
-
-// const deleteOldCaches = async () => {
-//   const cacheKeepList = ["v2"];
-//   const keyList = await caches.keys();
-//   const cachesToDelete = keyList.filter((key) => !cacheKeepList.includes(key));
-//   await Promise.all(cachesToDelete.map(deleteCache));
-// };
-
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(deleteOldCaches());
-// });
