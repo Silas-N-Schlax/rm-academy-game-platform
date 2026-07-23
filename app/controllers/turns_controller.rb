6ce9369039f5a9b5dbd430ef001a18b680f369fa
SilasNS <silasschlax@gmail.com>
2026-07-23 17:04:05 -0400

    refactor(turns): pass failed turn via locals, not ivar

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,11 +1,11 @@
 class TurnsController < ApplicationController
   def create
     @game = Game.find(params[:game_id])
-    @turn = @game.turn_class.new(turn_params)
-    if @turn.save
+    turn = @game.turn_class.new(turn_params)
+    if turn.save
       return redirect_to game_path(@game)
     end
-    render "games/show", layout: "application_no_sidebar", status: :unprocessable_content
+    render "games/show", layout: "application_no_sidebar", status: :unprocessable_content, locals: { turn: turn }
   end
 
   private
