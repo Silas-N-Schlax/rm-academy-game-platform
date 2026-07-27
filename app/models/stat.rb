d1dc7d9a9ab26513f108f880686c03f05a14638a
SilasNS <silasschlax@gmail.com>
2026-07-27 15:28:48 -0400

    fix(stat): scope aggregates to finished games

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,41 +1,47 @@
 class Stat
   def total_games(user)
-    user.games.size
+    finished_games(user).size
   end
 
   def total_wins(user)
-    Player.where(user_id: user.id, winner: true).size
+    finished_players(user, winner: true).size
   end
 
   def total_losses(user)
-    Player.where(user_id: user.id, winner: nil).size
+    finished_players(user, winner: [ false, nil ]).size
   end
 
   def total_average(user)
-    wins = total_wins(user)
-    total = total_games(user)
-    average(wins, total)
+    average(total_wins(user), total_games(user))
   end
 
   def total_games_by_game(user, type: "GoFishGame")
-    user.games.where(type: type).size
+    finished_games(user, type:).size
   end
 
   def total_wins_by_game(user, type: "GoFishGame")
-    Player.joins(:game).where(user_id: user.id, winner: true, games: { type: type }).size
+    finished_players(user, type:, winner: true).size
   end
 
   def total_losses_by_game(user, type: "GoFishGame")
-    Player.joins(:game).where(user_id: user.id, winner: nil, games: { type: type }).size
+    finished_players(user, type:, winner: [ false, nil ]).size
   end
 
   def total_average_by_game(user, type: "GoFishGame")
-    wins = total_wins_by_game(user, type: type)
-    total = total_games_by_game(user, type: type)
-    average(wins, total)
+    average(total_wins_by_game(user, type:), total_games_by_game(user, type:))
   end
 
   private
+
+  def finished_games(user, type: nil)
+    games = user.games.where.not(finished_at: nil)
+    type ? games.where(type:) : games
+  end
+
+  def finished_players(user, winner:, type: nil)
+    players = Player.joins(:game).where(user_id: user.id, winner:).merge(Game.where.not(finished_at: nil))
+    type ? players.where(games: { type: }) : players
+  end
 
   def average(wins, total)
     return 0.0 if total.zero?
--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -1,75 +1,92 @@
 require 'rails_helper'
 RSpec.describe Stat, type: :model do
-  let!(:user1) { create :user }
-  let!(:user2) { create :user }
-  let!(:game1) { create(:finished_game) }
-  let!(:game2) { create(:finished_game) }
-  let!(:game3) { create(:finished_game) }
-  let!(:game4) { create(:finished_game) }
-  let!(:game5) { create(:finished_game) }
-  let!(:game6) { create(:game, type: 'CrazyEightsGame') }
   let(:stat) { described_class.new }
-  before do
-    [ game1, game2, game3, game6 ].each do |game|
-      create(:player_as_winner, user: user1, game:)
-      create(:player, user: user2, game:)
-    end
-    create(:player, user: user1, game: game4)
-    create(:player_as_winner, user: user2, game: game4)
-  end
-  describe '#total_games' do
-    it 'returns the correct number' do
-      expected_output = 5
-      expect(stat.total_games(user1)).to eq expected_output
-    end
-  end
 
-  describe '#total_wins' do
-     it 'returns the correct number' do
-      expected_output = 4
-      expect(stat.total_wins(user1)).to eq expected_output
+  context 'with a mix of finished and unfinished games' do
+    let!(:user1) { create :user }
+    let!(:user2) { create :user }
+    let!(:game1) { create(:finished_game) }
+    let!(:game2) { create(:finished_game) }
+    let!(:game3) { create(:finished_game) }
+    let!(:game4) { create(:finished_game) }
+    let!(:game5) { create(:finished_game) }
+    let!(:game6) { create(:game, type: 'CrazyEightsGame') }
+    before do
+      [ game1, game2, game3, game6 ].each do |game|
+        create(:player_as_winner, user: user1, game:)
+        create(:player, user: user2, game:)
+      end
+      create(:player, user: user1, game: game4)
+      create(:player_as_winner, user: user2, game: game4)
     end
-  end
+    describe '#total_games' do
+      it 'returns the correct number' do
+        expected_output = 4
+        expect(stat.total_games(user1)).to eq expected_output
+      end
 
-  describe '#total_losses' do
-     it 'returns the correct number' do
-      expected_output = 1
-      expect(stat.total_losses(user1)).to eq expected_output
+      it 'does not count a game that has not finished' do
+        unfinished_game = create(:started_game)
+        create(:player, user: user1, game: unfinished_game)
+
+        expect(stat.total_games(user1)).to eq 4
+      end
     end
-  end
 
-  describe '#total_average' do
-     it 'returns the correct number' do
-      expected_output = 80.0
-      expect(stat.total_average(user1)).to eq expected_output
+    describe '#total_wins' do
+       it 'returns the correct number' do
+        expected_output = 3
+        expect(stat.total_wins(user1)).to eq expected_output
+      end
     end
-  end
 
-    describe '#total_games_by_game' do
-    it 'returns the correct number' do
-      expected_output = 4
-      expect(stat.total_games_by_game(user1)).to eq expected_output
+    describe '#total_losses' do
+       it 'returns the correct number' do
+        expected_output = 1
+        expect(stat.total_losses(user1)).to eq expected_output
+      end
+
+      it 'does not count a game that has not finished' do
+        unfinished_game = create(:started_game)
+        create(:player, user: user1, game: unfinished_game)
+
+        expect(stat.total_losses(user1)).to eq 1
+      end
     end
-  end
 
-  describe '#total_wins_by_game' do
-     it 'returns the correct number' do
-      expected_output = 3
-      expect(stat.total_wins_by_game(user1)).to eq expected_output
+    describe '#total_average' do
+       it 'returns the correct number' do
+        expected_output = 75.0
+        expect(stat.total_average(user1)).to eq expected_output
+      end
     end
-  end
 
-  describe '#total_losses_by_game' do
-     it 'returns the correct number' do
-      expected_output = 1
-      expect(stat.total_losses_by_game(user1)).to eq expected_output
+      describe '#total_games_by_game' do
+      it 'returns the correct number' do
+        expected_output = 4
+        expect(stat.total_games_by_game(user1)).to eq expected_output
+      end
     end
-  end
 
-  describe '#total_average_by_game' do
-     it 'returns the correct number' do
-      expected_output = 75
-      expect(stat.total_average_by_game(user1)).to eq expected_output
+    describe '#total_wins_by_game' do
+       it 'returns the correct number' do
+        expected_output = 3
+        expect(stat.total_wins_by_game(user1)).to eq expected_output
+      end
+    end
+
+    describe '#total_losses_by_game' do
+       it 'returns the correct number' do
+        expected_output = 1
+        expect(stat.total_losses_by_game(user1)).to eq expected_output
+      end
+    end
+
+    describe '#total_average_by_game' do
+       it 'returns the correct number' do
+        expected_output = 75
+        expect(stat.total_average_by_game(user1)).to eq expected_output
+      end
     end
   end
 end
